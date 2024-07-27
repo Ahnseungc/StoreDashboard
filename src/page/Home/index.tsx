529c671ca137ccf5454a0ea305d326baad1ab155
Ahnseungc <94547692+Ahnseungc@users.noreply.github.com>
2024-07-27 15:55:39 +0900

    Feat: 데이터 추가

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,5 +1,12 @@
 const Home = () => {
-  return <div>메인 페이지</div>;
+  return (
+    <section>
+      <div>매장 A</div>
+      <div>매장 B</div>
+      <div>매장 C</div>
+      <div>매장 D</div>
+    </section>
+  );
 };
 
 export default Home;
